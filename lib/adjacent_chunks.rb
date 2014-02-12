bb0087bc93b8371b7d61fd235b766191a08da1c2
Matthew Boston <matthew.boston@gmail.com>
2014-02-12 13:01:38 -0500

    use each_cons instead of AdjacentChunks

--- lib/adjacent_chunks.rb
+++
@@ -1,8 +0,0 @@
-class AdjacentChunks
-  def self.find(ary, size=1)
-    ary.each_with_index.map do |item, index|
-      chunk = ary.slice(index, size)
-      chunk if chunk.length == size
-    end.compact
-  end
-end
--- lib/ngrams.rb
+++ lib/ngrams.rb
@@ -1,9 +1,8 @@
-require 'adjacent_chunks'
 require 'words'
 
 class Ngrams
   def self.find(text, n=1)
-    AdjacentChunks.find(Words.find(text), n).map do |chunk|
+    Words.find(text).each_cons(n).map do |chunk|
       chunk.join(' ')
     end
   end
